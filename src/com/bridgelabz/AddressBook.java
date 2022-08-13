7d7123ef226d2a4c4846615ea5cdc8b9d42badc8
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-13 08:12:17 +0530

    Welcome note for Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,2 +1,7 @@
-package com.bridgelabz;public class addressbook {
+package com.bridgelabz;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
 }
