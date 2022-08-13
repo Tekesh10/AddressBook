057e8b61ab2c70cfc859e9df2b1f3e84a4cf910e
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-13 11:34:18 +0530

    Ability to delete the contact in the Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,42 +1,51 @@
 package com.bridgelabz;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class AddressBook {
     static Scanner sc = new Scanner(System.in);
     static List<Contacts> contactsList;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         contactsList = new ArrayList<>();
         showMenu();
     }
+
     static void showMenu() {
         System.out.println("Please select the option");
         System.out.println("1. Show Contact");
         System.out.println("2. Add new Contact");
         System.out.println("3. Update Contact");
-        System.out.println("4. Exit");
+        System.out.println("4. Delete Contact");
+        System.out.println("5. Exit");
         int selection = sc.nextInt();
         selectionFunction(selection);
     }
+
     static void selectionFunction(int selection) {
         switch (selection) {
             case 1 -> showContact();
             case 2 -> addContact();
             case 3 -> updateContact();
-            case 4 -> System.exit(0);
+            case 4 -> deleteContact();
+            case 5 -> System.exit(0);
             default -> showMenu();
         }
     }
+
     static void showContact() {
         System.out.println("Contacts");
         int count = 1;
         for (Contacts contacts : contactsList) {
-            System.out.println("Contact no."+ count + ". " + contacts);
+            System.out.println("Contact no." + count + ". " + contacts);
             count++;
         }
         showMenu();
     }
+
     static void addContact() {
         System.out.println("Add new Contact");
         System.out.print("Enter First Name : ");
@@ -60,6 +69,7 @@ public class AddressBook {
         System.out.println(contacts);
         showMenu();
     }
+
     static void updateContact() {
         System.out.println("Enter the name of the Person to update the contact details");
         String name = sc.next();
@@ -86,14 +96,28 @@ public class AddressBook {
                 contacts.setCity(city);
                 contacts.setState(state);
                 contacts.setZipcode(zipcode);
-                System.out.println("Contact Updated");
+                System.out.println("Contact Updated in the Address Book");
+            }
+        }
+        showMenu();
+    }
+
+    static void deleteContact() {
+        System.out.println("Enter the name of the Person to Delete the contact details");
+        String name = sc.next();
+        for (Contacts contacts : contactsList) {
+            if (contacts.first_name.equals(name)) {
+                contactsList.remove(contacts);
+                System.out.println(name + " is deleted from the Address Book");
             }
         }
         showMenu();
     }
 }
+
 class Contacts {
     String first_name, last_name, city, state, email, phone_number, zipcode;
+
     public Contacts(String first_name, String last_name, String phone_number, String email, String city, String state, String zipcode) {
         this.first_name = first_name;
         this.last_name = last_name;
@@ -103,27 +127,35 @@ class Contacts {
         this.state = state;
         this.zipcode = zipcode;
     }
+
     public void setFirstname(String first_name) {
         this.first_name = first_name;
     }
+
     public void setLastname(String last_name) {
         this.last_name = last_name;
     }
+
     public void setPhone(String phone_number) {
         this.phone_number = phone_number;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
+
     @Override
     public String toString() {
         return "First Name : '" + first_name + "', Last Name : '" + last_name + "', Phone Number : '" + phone_number + "', email ID : '" + email + "', City : '" + city + "', State : '" + state + "', Zipcode : '" + zipcode + "'";
